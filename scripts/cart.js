0db803f9ded1a743e5d677c1d84648d48a4f2d27
Md. Jobairul Islam <140498838+JoIslam@users.noreply.github.com>
2023-08-30 03:07:08 +0530

    Update cart.js

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,9 +1,8 @@
 export let cart=JSON.parse(localStorage.getItem('cart'));
 //export let cart=[];
-/*if(!cart){
-	cart=[{prodId:'shirt3',quantity:1},
-{prodId:'shirt4',quantity:1}];
-}*/
+if(!cart){
+	cart=[];
+}
 
 export function addToCart(prodId){
       cart.push({prodId:prodId,quantity:1});
